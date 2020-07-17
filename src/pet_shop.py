46170c760f65df7844221139835e120d585de5ae
Timothy Saudners <timsaunders88@gmail.com>
2020-07-17 11:52:56 +0100

    added function get_pets_by_breed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -16,3 +16,10 @@ def increase_pets_sold(pet_shop, value):
 
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pets = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets.append(pet)
+    return pets
